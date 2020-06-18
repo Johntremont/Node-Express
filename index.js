e12ca12a74b143958dcaa64993a9e7507c6fe6a5
johntremont <piloto2810@gmail.com>
2020-06-18 16:41:28 -0400

    rest

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-/*
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.status = 200;
-    res.setHeader('content-type', 'text/plain');
-    res.end("Hello WOrld");
-});
-
-server.listen(3000, () => {
-    console.log('Server on port 3000')
-})
-
-*/
-
 const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('PETICION GET RECIBIDA');
+});
+
+app.post('/login', (req, res) => {
+    res.send('PETICION POST RECIBIDA');
+});
+
+app.put('/show', (req, res) => {
+    res.send('PETICION PUT RECIBIDA');
+});
+
+app.delete('/delete', (req, res) => {
+    res.send('PETICION DELETE RECIBIDA');
+});
+
+app.listen(3000, () => {
+    console.log('server on port 3000');
+});
